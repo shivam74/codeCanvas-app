d390fbc1674378c353e1b37dfdcf50a1dce65752
Shivam <shivamraghuwanshi741@gmail.com>
2025-12-20 15:41:31 +0530

    single post page completed

--- src/routes/SinglePostPage.jsx
+++ src/routes/SinglePostPage.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import IKImage from "../components/IKImage";
 import PostMenuActions from "../components/PostMenuActions";
 import Search from "../components/Search";
+import Comments from "../components/Comments";
 
 const SinglePostPage = () => {
   return (
